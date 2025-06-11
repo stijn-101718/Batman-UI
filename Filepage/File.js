0b4babb21b9c8790e13b36025c4f719b4e1e7e4d
101727 <101727@glr.nl>
2025-06-11 10:15:07 +0000

    commit 11 jun

--- Filepage/File.js
+++ Filepage/File.js
@@ -1,9 +1,5 @@
-    let fileIndex = 1;
-
-    function nextFile() {
-      fileIndex++;
-      document.querySelector('.header').innerText = `Case File 000${fileIndex}_2010.02.20_01`;
-    }
+    let currentText = 0;
+    const textBoxes = document.querySelectorAll(".text-box");
 
     function searchFiles() {
       const input = document.getElementById("searchInput").value.toLowerCase();
@@ -13,3 +9,13 @@
         box.style.display = alt.includes(input) ? "block" : "none";
       });
     }
+
+    function cycleText() {
+      textBoxes[currentText].classList.remove("visible");
+      textBoxes[currentText].classList.add("hidden");
+      currentText = (currentText + 1) % textBoxes.length;
+      textBoxes[currentText].classList.remove("hidden");
+      textBoxes[currentText].classList.add("visible");
+    }
+
+    setInterval(cycleText, 3000);
